60bf372d27789bf1e4a6b2ae6f988327cfed65a7
michal-jewczuk <michal.jewczuk@outlook.com>
2021-01-29 08:38:54 +0100

    Day 5 - part 2

--- 2019/src/net/jewczuk/aoc/days/Day05.java
+++ 2019/src/net/jewczuk/aoc/days/Day05.java
@@ -24,11 +24,14 @@ public class Day05 extends DayRunner {
         Outputter.INSTANCE.displayResuls(className, 1, 0, ResultType.EXERCISE.toString());
         solvePart1(exerciseNumericData);
         Outputter.INSTANCE.displayResuls(className, 2, 0, ResultType.EXERCISE.toString());
+        solvePart2(exerciseNumericData);
         Outputter.INSTANCE.displayBreakLine(5);
     }
 
     private void transformInputDataToList() {
-        exerciseNumericData = Arrays.asList(FileReader.INSTANCE.loadStringData("d05_exercise.txt").get(0).split(",")).stream()
+        exerciseNumericData = Arrays.asList(
+                FileReader.INSTANCE.loadStringData("d05_exercise.txt").get(0).split(",")
+        ).stream()
                 .map(el -> Integer.parseInt(el))
                 .collect(Collectors.toList());
     }
@@ -36,6 +39,13 @@ public class Day05 extends DayRunner {
     private void solvePart1(List<Integer> initialData) {
         List<Integer> allowedCodes = Arrays.asList(1, 2, 3, 4);
         int initialInput = 1;
+
+        applyOpcodes(initialData, initialInput, allowedCodes);
+    }
+
+    private void solvePart2(List<Integer> initialData) {
+        List<Integer> allowedCodes = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        int initialInput = 5;
 
         applyOpcodes(initialData, initialInput, allowedCodes);
     }
@@ -62,13 +72,22 @@ public class Day05 extends DayRunner {
 
     private int applyOperation(int oppCode, int idx, List<Integer> output, int initialInput) {
         int step = getInstructionLength(oppCode % 10);
+        int singleDigit = oppCode % 10;
 
         List<Integer> elements = output.subList(idx+1, idx+step);
-        System.out.format("Applying opp code of %d on values %s \n", oppCode, elements);
-        if (oppCode % 10 == 3) {
+//        System.out.format("Applying opp code of %d on values %s \n", oppCode, elements);
+        if (singleDigit == 3) {
             applyInputOperation(idx, output, initialInput);
-        } else if (oppCode % 10 == 4) {
+        } else if (singleDigit == 4) {
             applyOutputOperation(oppCode, idx, output);
+        } else if (singleDigit == 5) {
+            return applyJumpIfTrueOperation(oppCode, idx, output);
+        } else if (singleDigit == 6) {
+            return applyJumpIfFalseOperation(oppCode, idx, output);
+        } else if (singleDigit == 7) {
+            applyLessThanOperation(oppCode, idx, output);
+        } else if (singleDigit == 8) {
+            applyEqualsOperation(oppCode, idx, output);
         } else {
             applyReplaceOperation(oppCode, idx, output);
         }
@@ -79,6 +98,8 @@ public class Day05 extends DayRunner {
     private int getInstructionLength(int oppCode) {
         if (oppCode == 3 || oppCode == 4) {
             return 2;
+        } else if (oppCode == 5 || oppCode == 6) {
+            return 3;
         } else {
             return 4;
         }
@@ -94,7 +115,9 @@ public class Day05 extends DayRunner {
         if (oppCode == 4) {
             value = output.get(value);
         }
-        System.out.println("Output for current test is: " + value);
+        if (value != 0) {
+            System.out.println("Current output is: " + value);
+        }
     }
 
     private void applyReplaceOperation(int oppCode, int idx, List<Integer> output) {
@@ -105,6 +128,56 @@ public class Day05 extends DayRunner {
         int result = firstElement + secondElement;
         if (oppCode % 10 == 2) {
             result = firstElement * secondElement;
+        }
+
+        output.set(output.get(idx + 3), result);
+    }
+
+    private int applyJumpIfTrueOperation(int oppCode, int idx, List<Integer> output) {
+        int [] modes = extractModes(oppCode);
+        int firstElement = getValueByMode(modes[0], output.get(idx + 1), output);
+        int secondElement = getValueByMode(modes[1], output.get(idx + 2), output);
+
+        if (firstElement != 0) {
+            return secondElement;
+        } else {
+            return idx + 3;
+        }
+    }
+
+    private int applyJumpIfFalseOperation(int oppCode, int idx, List<Integer> output) {
+        int [] modes = extractModes(oppCode);
+        int firstElement = getValueByMode(modes[0], output.get(idx + 1), output);
+        int secondElement = getValueByMode(modes[1], output.get(idx + 2), output);
+
+        if (firstElement == 0) {
+            return secondElement;
+        } else {
+            return idx + 3;
+        }
+    }
+
+    private void applyLessThanOperation(int oppCode, int idx, List<Integer> output) {
+        int [] modes = extractModes(oppCode);
+        int firstElement = getValueByMode(modes[0], output.get(idx + 1), output);
+        int secondElement = getValueByMode(modes[1], output.get(idx + 2), output);
+
+        int result = 0;
+        if (firstElement < secondElement) {
+            result = 1;
+        }
+
+        output.set(output.get(idx + 3), result);
+    }
+
+    private void applyEqualsOperation(int oppCode, int idx, List<Integer> output) {
+        int [] modes = extractModes(oppCode);
+        int firstElement = getValueByMode(modes[0], output.get(idx + 1), output);
+        int secondElement = getValueByMode(modes[1], output.get(idx + 2), output);
+
+        int result = 0;
+        if (firstElement == secondElement) {
+            result = 1;
         }
 
         output.set(output.get(idx + 3), result);
