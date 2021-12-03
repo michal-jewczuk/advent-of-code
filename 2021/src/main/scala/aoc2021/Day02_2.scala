1ac9524f6abf0b665d3093072dac449f8e22f0d9
michal-jewczuk <michal.jewczuk@outlook.com>
2021-12-03 09:26:53 +0100

    Refactor day 2 part 2 to use recursive

--- 2021/src/main/scala/aoc2021/Day02_2.scala
+++ 2021/src/main/scala/aoc2021/Day02_2.scala
@@ -1,33 +1,34 @@
 package aoc2021
 
+import scala.annotation.tailrec
+
 object Day02_2 extends App {
 
   val directionsExample = loadData("d02e.txt").get
-  val directions= loadData("d02.txt").get
+    .map(line => line.split(" "))
+    .map(a => (a(0), a(1).toInt))
+  val directions = loadData("d02.txt").get
+    .map(line => line.split(" "))
+    .map(a => (a(0), a(1).toInt))
 
-  def extractValue(dir: String, line: String): Int = {
-    line.substring(dir.length + 1).toInt
-  }
+  def calculatePosition(directions: List[(String, Int)]): (Int, Int) = {
+    @tailrec
+    def accumulate(all: List[(String, Int)], accumulators: (Int, Int, Int)): (Int, Int) = {
+      if (all.isEmpty) (accumulators._1, accumulators._2)
+      else accumulate(all.tail, adjustForwardDepthAim(all.head, accumulators))
+    }
 
-  def calculatePosition(directions: List[String]): (Int, Int) = {
-    var forward = 0
-    var depth = 0
-    var aim = 0
-    directions.foreach(d => {
-      if (d.startsWith("down")) aim = aim + extractValue("down", d)
-      else if (d.startsWith("up")) aim = aim - extractValue("up", d)
-      else {
-        val fVal = extractValue("forward", d)
-        forward = forward + fVal
-        depth = depth + fVal * aim
-      }
-    })
+    def adjustForwardDepthAim(command: (String, Int), metrics: (Int, Int, Int)): (Int, Int, Int) = {
+      if (command._1.equals("down")) (metrics._1, metrics._2, metrics._3 + command._2)
+      else if (command._1.equals("up")) (metrics._1, metrics._2, metrics._3 - command._2)
+      else (metrics._1 + command._2, metrics._2 + command._2 * metrics._3, metrics._3)
+    }
 
-    (forward, depth)
+    accumulate(directions, (0, 0, 0))
   }
 
   println(calculatePosition(directionsExample))
-  
+
   val position = calculatePosition(directions)
   println(position._1 * position._2)
 }
