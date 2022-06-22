4ebfacd44248912d500efa21291164d6f174a550
michal-jewczuk <michal.jewczuk@outlook.com>
2022-06-22 09:34:17 +0200

    Solve day 22 part 2

--- 2021/src/main/scala/aoc2021/Day22_2.scala
+++ 2021/src/main/scala/aoc2021/Day22_2.scala
@@ -1,42 +1,72 @@
 package aoc2021
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 
 object Day22_2 extends App {
 
-  val linesE1 = loadData("d22e1.txt").get
-  val linesE = loadData("d22e3.txt").get
-  val lines = loadData("d22.txt").get
+  val dataE1 = processData("d22e1.txt")
+  val dataE2 = processData("d22e2.txt")
+  val dataE3 = processData("d22e3.txt")
+  val data = processData("d22.txt")
 
-  val inputsE1 = linesE1.map(process)
-  val inputsE = linesE.map(process)
-  val inputs = lines.map(process)
+  case class Cube(x: (Int, Int), y: (Int, Int), z: (Int, Int)) {
+    def apply(): Long = 1L * (x._1 to x._2).size * (y._1 to y._2).size * (z._1 to z._2).size
+  }
+  case class Command(state: String, cube: Cube) {
+    def isOn: Boolean = state.equals("on")
+  }
+  case class Intersection(level: Int, cube: Cube) {
+    def apply(): Long = {
+      val sign = if (level % 2 == 0) 1 else -1
+      cube() * sign
+    }
+    def isNotEmpty: Boolean = cube() > 0
+  }
 
-  def process(line: String): (String, (Int, Int), (Int, Int), (Int, Int)) = {
+  def processData(fileName: String): List[Command] = {
+    loadData(fileName).get.map(process)
+  }
+
+  def process(line: String): Command = {
     val state = line.substring(0, 3).trim
     val elements = line.substring(3).trim.split(",")
       .map(_.substring(2))
       .map(_.split("\\.\\.").toList)
       .map(l => (l.head.toInt, l.tail.head.toInt))
-
-    (state, elements(0), elements(1), elements(2))
+    new Command(state, new Cube(elements(0), elements(1), elements(2)))
   }
 
-  def generateCubesCount(first: (Int, Int), second: (Int, Int), third: (Int, Int)): Long = {
-    1l * (first._1 to first._2).size * (second._1 to second._2).size * (third._1 to third._2).size
+  def findIntersection(cube1: Cube, cube2: Cube): Intersection = {
+    val empty = new Intersection(0, new Cube((-1, -2), (-1, -2), (-1, -2)))
+    if (cube2.x._2 < cube1.x._1 || cube2.x._1 > cube1.x._2) return empty
+    if (cube2.y._2 < cube1.y._1 || cube2.y._1 > cube1.y._2) return empty
+    if (cube2.z._2 < cube1.z._1 || cube2.z._1 > cube1.z._2) return empty
+
+    val x = (Math.max(cube1.x._1, cube2.x._1), Math.min(cube1.x._2, cube2.x._2))
+    val y = (Math.max(cube1.y._1, cube2.y._1), Math.min(cube1.y._2, cube2.y._2))
+    val z = (Math.max(cube1.z._1, cube2.z._1), Math.min(cube1.z._2, cube2.z._2))
+
+    new Intersection(1, new Cube(x, y, z))
   }
 
-  def findCommonArea(original: ((Int, Int), (Int, Int), (Int, Int)), ext: ((Int, Int), (Int, Int), (Int, Int))): ((Int, Int), (Int, Int), (Int, Int)) = {
-    val (x1, y1, z1) = original
-    val (x2, y2, z2) = ext
-
-    if (x2._2 < x1._1 || x2._1 > x1._2) return ((-1, -2), (-1, -2), (-1, -2))
-    if (y2._2 < y1._1 || y2._1 > y1._2) return ((-1, -2), (-1, -2), (-1, -2))
-    if (z2._2 < z1._1 || z2._1 > z1._2) return ((-1, -2), (-1, -2), (-1, -2))
-
-    ((Math.max(x1._1, x2._1), Math.min(x1._2, x2._2)), (Math.max(y1._1, y2._1), Math.min(y1._2, y2._2)), (Math.max(z1._1, z2._1), Math.min(z1._2, z2._2)))
+  def findIntersection(intersection: Intersection, cube: Cube): Intersection = {
+    new Intersection(intersection.level + 1, findIntersection(intersection.cube, cube).cube)
   }
 
+  def findSolution(commands: List[Command]): Long = {
+    @tailrec def countUntil(left: List[Command], processed: List[Cube], intersections: List[Intersection]): Long = {
+      if (left.isEmpty) processed.map(_()).sum + intersections.map(_()).sum
+      else {
+        val cmd = left.head
+        val newIntersec = processed.map(findIntersection(_, cmd.cube)) ++ intersections.map(findIntersection(_, cmd.cube))
+        countUntil(left.tail, if (cmd.isOn) processed :+ cmd.cube else processed, intersections ++ newIntersec.filter(_.isNotEmpty))
+      }
+    }
+    countUntil(commands, List.empty, List.empty)
+  }
 
+  println((findSolution(dataE1), 39))
+  println((findSolution(dataE2), 590784))
+  println(findSolution(dataE3))
+  println(findSolution(data))
 }
